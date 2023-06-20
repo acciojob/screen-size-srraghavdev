30d0c813fa621cae28816a544a49de79098ff1c6
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 03:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+addEventListener("resize",(event)=>{
+	const fg = document.getElementById("hf")
+	const children= fg.children
+	children[1].innerText=(event.target.innerHeight)
+	children[0].innerText=(event.target.innerWidth)
+
+
+})
