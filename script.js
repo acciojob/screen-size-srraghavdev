05b4b30fdfbf543b26abdb90b42e519af16fb85e
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 17:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 //your JS code here. If required.
-const widthref= document.getElementsByTagName("h1")[0]
-const heightref= document.getElementsByTagName("h1")[1]
-let width=window.innerWidth
-let height=window.innerHeight
-widthref.innerText="Width:"+width
-heightref.innerText="Height:"+height
-addEventListener("resize",()=>{
-	let width=window.innerWidth
-	let height=window.innerHeight
-	widthref.innerText="Width:"+width
-	console.log(widthref)
-	heightref.innerText="Height:"+height
-})
+const sizeInfo = document.querySelector('#sizeInfo');
+const width = sizeInfo.querySelector('h1:first-child');
+const height = sizeInfo.querySelector('h1:last-child');
+
+function setScreenSize() {
+width.innerText = `Width: ${window.innerWidth}px`;
+height.innerText = `Height: ${window.innerHeight}px`;
+}
+
+window.addEventListener('resize', setScreenSize);
+
+setScreenSize();
