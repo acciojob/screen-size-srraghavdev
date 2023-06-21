f51cab403ea9c25661efce54dfd58282b70975d9
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 16:57:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //your JS code here. If required.
 const widthref= document.getElementsByTagName("h1")[0]
 const heightref= document.getElementsByTagName("h1")[1]
+let width=event.target.innerWidth
+let height=event.target.innerHeight
+widthref.innerText="Width:"+width
+heightref.innerText="Height:"+height
 addEventListener("resize",(event)=>{
 	let width=event.target.innerWidth
 	let height=event.target.innerHeight
